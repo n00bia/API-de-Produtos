b0c572f444a3233bd0b41986ce4c1ee6117e5d0b
Nubia Santos <noobiacristine@gmail.com>
2024-06-08 16:38:45 -0300

    Add project files.

---
+++ Controllers/ProdutoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_de_Produtos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+    }
+}
---
+++ Models/ProdutoModel.cs
@@ -0,0 +1,10 @@
+﻿namespace API_de_Produtos.Models
+{
+    public class ProdutoModel
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Tipo { get; set; }
+        public decimal? PrecoUnitario { get; set; }
+    }
+}
